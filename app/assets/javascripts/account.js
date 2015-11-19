9b3fca6c77c4eaf73fda77a5deaa964192e39c1f
Jonathan Arp <jarp@nd.edu>
2015-11-19 17:52:42 -0500

    fixed js issue story partial now responds correctly to settings

--- app/assets/javascripts/account.js
+++ app/assets/javascripts/account.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 
   $('span.show-subscribe').click(function(){
-    console.log('click')
-    console.log($(this).closest('.row'))
-    console.log($(this).closest('.row').next('.toggable'))
-    $(this).closest('.row').next('.action-panel').slideToggle();
+    //console.log('click')
+    //console.log($(this).closest('.row'))
+    //console.log($(this).closest('.row').next('.toggable'))
+    $(this).closest('.row').next('.toggable').slideToggle();
   })
 
 })
