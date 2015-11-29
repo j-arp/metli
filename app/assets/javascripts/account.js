c72096bc85f8089aa5e3339dc29e4821a42a6230
Jonathan Arp <jarp@nd.edu>
2015-11-29 00:03:19 -0500

    pipe

--- app/assets/javascripts/account.js
+++ app/assets/javascripts/account.js
@@ -1,10 +1,5 @@
 $(document).ready(function(){
-
   $('span.show-subscribe').click(function(){
-    //console.log('click')
-    //console.log($(this).closest('.row'))
-    //console.log($(this).closest('.row').next('.toggable'))
     $(this).closest('.row').next('.toggable').slideToggle();
   })
-
 })
--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -12,9 +12,3 @@ $(document).ready(function(){
   });
 
 })
-
-// $(window).scroll(function(){
-//   if  ($(window).scrollTop() == $(document).height() - $(window).height()){
-//         //console.log($('nav').offset() )
-//   }
-// });
