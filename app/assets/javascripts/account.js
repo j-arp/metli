07777471abfd1d498a1aab73cab69c786de65396
Jonathan Arp <jarp@nd.edu>
2015-12-02 17:19:34 -0500

    ui nix turbo links

--- app/assets/javascripts/account.js
+++ app/assets/javascripts/account.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
+  console.log('jq loaded')
   $('span.show-subscribe').click(function(){
+
+    console.log('click')
     var story_id = $(this).attr('id')
     var selector = $('.toggable').filter('#'+ story_id)
-    console.log('click')
     console.log(selector)
     selector.slideToggle();
   })
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
-//= require turbolinks
 //= require ckeditor/init
 //= require_tree .
 //
