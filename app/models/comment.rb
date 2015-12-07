915d6eb2e9c96c819be92cd4e2fe43dbf0ae57a7
Jonathan Arp <jarp@nd.edu>
2015-12-07 15:09:06 -0500

    order comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :chapter
 
-  scope :newer, -> { order('created_at asc')}
-  scope :older, -> { order('created_at desc')}
+  scope :newer, -> { order('created_at desc')}
+  scope :older, -> { order('created_at asc')}
 
   validates :content, :user, :chapter, presence: true
 end
