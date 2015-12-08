d6da8133b04a8ffdd2e5bf74c087b8e4e4e2318f
Jonathan Arp <jarp@nd.edu>
2015-12-08 10:40:22 -0500

    s3 config?

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,12 @@
 CarrierWave.configure do |config|
-  # config.fog_provider = 'fog/aws'                        # required
+  config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
     aws_access_key_id:     "#{ENV['export WN_AWS_ID']}",                        # required
     aws_secret_access_key: "#{ENV['export WN_AWS_KEY']}"                         # required
+    region:                'us-east-1',                  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'what-next'
-  config.fog_public     = true                            # required
+  config.fog_directory  = 'what-next'                          # required
+  config.fog_public     = true                                        # optional, defaults to true
+  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
