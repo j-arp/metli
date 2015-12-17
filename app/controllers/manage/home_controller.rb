3e0f40b92a143a7bbe4887c6250eaf9778b629f4
Jonathan Arp <jarp@nd.edu>
2015-12-17 16:36:04 -0500

    order of newest nav link

--- app/controllers/manage/home_controller.rb
+++ app/controllers/manage/home_controller.rb
@@ -2,7 +2,7 @@ module Manage
   class HomeController < SuperUserController
     def index
       @active_users = User.by_activity.limit(10)
-      @newest_users = User.order('created_at DESC').limit(10)
+      @newest_users = User.unscoped.where(deleted: false).order('created_at DESC').limit(10)
       @recent_stories = Story.by_activity.limit(10)
     end
 end
