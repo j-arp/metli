8f7195c630527955c952b8c1841d8dc2c666353e
Jonathan Arp <jarp@nd.edu>
2015-12-19 00:29:02 -0500

    bashboard

--- app/controllers/manage/home_controller.rb
+++ app/controllers/manage/home_controller.rb
@@ -2,8 +2,8 @@ module Manage
   class HomeController < SuperUserController
     def index
       @active_users = User.by_activity.limit(10)
-      @newest_users = User.unscoped.where(deleted: false).order('created_at DESC').limit(10)
-      @recent_stories = Story.by_activity.limit(10)
+      @newest_users = User.unscoped.active.order('created_at DESC').limit(10)
+      @recent_stories = Story.unscoped.active.by_activity.limit(10)
     end
 end
 end
