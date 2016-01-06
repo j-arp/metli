1661378887b4b062765f341acbb618e4b9c7299b
Jonathan Arp <jarp@nd.edu>
2016-01-05 22:44:34 -0500

    bunch of copy and ui changes about/home are more merged like

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
+    if session[:user_id]
+      redirect_to choose_story_path
+    else
+      render :about
+    end
   end
 
   def about
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,11 +2,25 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
 
+
   describe "GET #index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+    it "renders the about page" do
+      get :index
+      expect(response).to render_template(:about)
+    end
+
+    it "redirects to choosing a story if logged in" do
+      user = FactoryGirl.create(:user)
+      get :index,{},{user_id: user.id}
+      expect(response).to redirect_to(choose_story_path)
+    end
+
+
   end
 
   describe "GET #about" do
