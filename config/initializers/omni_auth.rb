297e8b24fc7cd2886216a426644359eca331f659
Jonathan Arp <jarp@nd.edu>
2016-01-11 23:19:23 -0500

    scopes

--- config/initializers/omni_auth.rb
+++ config/initializers/omni_auth.rb
@@ -6,6 +6,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 end
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV["FB_ID"], ENV["FB_SECRET"],
-  :scope => ['public_profile,email']
+  :scope => ['public_profile','email']
 
 end
